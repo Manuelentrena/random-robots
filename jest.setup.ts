f9b7ea68c01914b34aa1c2436b478aab6c73c64e
entrenadev <manuel.entrena@gmail.com>
2025-06-26 22:35:07 +0200

    fix(test): :bug: Fixed bug intersectionObserver like global mock

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,1 +1,13 @@
+// jest.setup.ts
 import '@testing-library/jest-dom';
+
+// Mock IntersectionObserver
+global.IntersectionObserver = jest.fn(() => ({
+  observe: jest.fn(),
+  unobserve: jest.fn(),
+  disconnect: jest.fn(),
+  takeRecords: jest.fn(),
+  root: null,
+  rootMargin: '',
+  thresholds: [],
+}));
