307e14a4d6176778494ea87d37b4a34c8ac2d866
entrenadev <manuel.entrena@gmail.com>
2025-06-26 19:44:09 +0200

    feat(config): :sparkles: Add initial configuration for RandomUser API

---
+++ config/initial.ts
@@ -0,0 +1,5 @@
+export const initialConfig = {
+  page: 1,
+  results: 10,
+  url: 'https://randomuser.me/api/',
+};
--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import { useState } from 'react';
 import './index.css';
 import './App.css';
 import 'nes.css/css/nes.min.css';
@@ -6,22 +5,18 @@ import { RandomUserRobotRepository } from '@/core/infrastructure/random-user.api
 import { GetRobotsUseCase } from '@/core/application/get-robots.usecase';
 import { RobotProvider } from '@/store/robot.provider';
 import { RobotsList } from './components/RobotList';
+import { initialConfig } from '@/config/initial';
 
-const repo = new RandomUserRobotRepository('https://randomuser.me/api/');
+const repo = new RandomUserRobotRepository(initialConfig.url);
 const serviceRobot = new GetRobotsUseCase(repo);
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <RobotProvider service={serviceRobot}>
       <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100 text-gray-800 p-4">
-        <div className="bg-white shadow-md rounded-2xl p-8 w-full max-w-md text-center">
+        <div className="bg-white shadow-md rounded-2xl p-8 w-full max-w-2xl md:max-w-3xl lg:max-w-5xl xl:max-w-6xl text-center h-[90vh] md:h-auto">
           <h1 className="text-3xl font-bold text-custom-blue mb-4">Random Robots</h1>
           <RobotsList />
-          <button onClick={() => setCount(count + 1)} type="button" className="nes-btn is-primary">
-            Count is: {count}
-          </button>
         </div>
       </div>
     </RobotProvider>
