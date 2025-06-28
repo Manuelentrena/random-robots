2e5d540bc8ad356d0b2b0b1af6b6b6f2d52204ad
entrenadev <manuel.entrena@gmail.com>
2025-06-28 12:44:41 +0200

    test(config): 🧪 Update jest.setup.ts to include additional polyfills for Fetch API and BroadcastChannel

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,5 +1,8 @@
 // jest.setup.ts
 import '@testing-library/jest-dom';
+import 'whatwg-fetch';
+
+/* eslint-disable @typescript-eslint/no-require-imports */
 
 // Mock IntersectionObserver
 global.IntersectionObserver = jest.fn(() => ({
@@ -12,7 +15,30 @@ global.IntersectionObserver = jest.fn(() => ({
   thresholds: [],
 }));
 
-// jest.setup.js
-/* eslint-disable @typescript-eslint/no-require-imports */
+// Polyfills para Node.js
 global.TextEncoder = require('util').TextEncoder;
 global.TextDecoder = require('util').TextDecoder;
+
+// Polyfill para TransformStream
+if (typeof TransformStream === 'undefined') {
+  global.TransformStream =
+    require('stream/web').TransformStream || require('web-streams-polyfill').TransformStream;
+}
+
+// Polyfills para Fetch API
+if (typeof Response === 'undefined') {
+  const { Response, Request, Headers } = require('node-fetch');
+  global.Response = Response;
+  global.Request = Request;
+  global.Headers = Headers;
+}
+
+// Polyfill para BroadcastChannel
+class MockBroadcastChannel {
+  constructor() {}
+  postMessage() {}
+  close() {}
+  addEventListener() {}
+  removeEventListener() {}
+}
+global.BroadcastChannel = MockBroadcastChannel as any;
